2868cbce3d9e9fffb8e97f366f72a06ad154807a
ojas1 <mohrilop@gmail.com>
2016-06-29 16:42:51 +0530

    started working on doubly ll

---
+++ linkedlist/doublylinkedlist.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+//node of the doubly linked list
+typedef struct node{
+	int data;
+	struct node* prev;
+	struct node* next;
+}Node;
+
+// to insert a node at position 
+// to insert a node after given data
+// to insert a node before given data
+// to delete a node at position 
+// to delete a node after given data
+// to delete a node before given data
+// to replace node at given position 
+// to replace a node having some data with a new node having some other data
+// to search for node with given data
+// to print the entire list 
+// to reverse the list 
+
+int main(){
+	//head of the linked list
+	Node *head=NULL;
+	
+	/*getting user input for operation selection 
+	  1. insert at position 
+	  2. insert after 
+	  3. insert before 
+	  4. delete node at position 
+	  5. delete node after 
+	  6. delete node before
+	  7. replace node
+	  8. search for data
+	  9. print entire list 
+	  10. reverse list
+	  */
+	  
+	return 0;
+}
