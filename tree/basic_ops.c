926d4910058cf547f3cd106ce8544850450ba23a
ojas1 <mohrilop@gmail.com>
2016-07-18 08:51:38 +0530

    ready for insertion testing 1

--- tree/basic_ops.c
+++ tree/basic_ops.c
@@ -8,7 +8,7 @@ typedef struct {int id; float value;}Ndtype;
 typedef enum {SUCCESS, FAILURE, NOACT}_status_code;
 
 /* ERROR codes */
-typedef enum {NPTR}_error_code;
+typedef enum {NPPTR, NPTR, MMAL, UNID}_error_code;
 
 /* tree node */
 typedef struct node{
@@ -24,11 +24,20 @@ typedef struct node{
 }TNode;
 
 // provide error information
-void indicate_error(_error_code e, char type[]){
+void indicate_error(_error_code e){
 	printf("\n");
-	if(e = NPTR){
-		if(type == "pTNode")
-		printf("ERROR: Expected Tree Node Pointer, Received NULL pointer");		
+	switch(e) {
+	case NPTR:
+		printf("ERROR: Expected TNode Pointer, Received NULL Pointer");
+		break;		
+	case NPPTR:
+		printf("ERROR: Expected Pointer to TNode Pointer, Received NULL Pointer");
+		break;
+	case MMAL:
+		printf("ERROR: Memory Allocation Failed");	
+		break;
+	default:
+		printf("ERROR: Unidentified Error");
 	}
 	printf("\n");
 }
@@ -37,31 +46,62 @@ void indicate_error(_error_code e, char type[]){
 // create new tree node, and return pointer to it.
 TNode* create_TNode(NDtype data){
 
-	// allocate memeory 
+	// allocate memory 
 	TNode* tnode= malloc(sizeof(TNode));
 
-	// insert data
-	(tnode->data).id = data.id;
-	(tnode->data).value = data.value;
+	// if memory allocation fails, indicate the failure and return.
+	if(tnode == NULL){indicate_error(MMAL);}
+
+	// if memory allocation succeeds, carry on with further opertations.	
+	else{
+		// insert data
+		(tnode->data).id = data.id;
+		(tnode->data).value = data.value;
 	
-	tnode->left = NULL;
-	tnode->right = NULL;
+		tnode->left = NULL;
+		tnode->right = NULL;
+	}
 	
+	// return either a pointer to the successfully created TNode, or a NULL pointer.
 	return tnode;
 }
 
 
+// find parent of node to be inserted in the tree
+TNode* find_parent(TNode* root, int id){
+	return root;
+}
+
+
 // insert a node in the tree, (binary tree).
-_status_code insert(TNode* root, Ndtype data){
+_status_code insert(TNode** rootref, Ndtype data){
 	_status_code ret_code = SUCCESS;
 	
-	// validate root
-	if(root == NULL) {indicate_error(NPTR, "pTNode");}
+	// validate pointer to pointer to root
+	if(rootref == NULL) {indicate_error(NPPTR);}
 	
-	//if root is valid	
+	//if double pointer is valid
 	else {
 		// create new node
 		TNode* new_TNode = create_TNode(data);
+
+		// if tnode creation fails, return with FAILURE code
+		if(new_TNode == NULL) ret_code = FAILURE;
+		
+		// if tnode creation succeeds
+		else {
+			// if the root pointer is NULL, i.e. the tree is empty, the new node becomes the root
+			if((*rootref) == NULL) (*rootref) = new_TNode;
+			
+			/* else find the proper parent for the new node, 
+			decide weather it should be left or right child and insert*/
+			TNode* parent = find_parent(*rootref, data.id);
+
+			// insert 
+			if(parent != NULL){
+				parent->left = newTNode;
+			}			
+		}		
 	} 	 
 	return ret_code;
 }
